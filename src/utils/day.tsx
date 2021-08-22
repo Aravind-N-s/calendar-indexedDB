f48857e791d05e2ef1e1f1f13d22dc837e3e1698
Aravind <aravind1808@gmail.com>
2021-08-22 19:35:35 +0530

    [Eslint][Func] Fixing Eslint Error

--- src/utils/day.tsx
+++ src/utils/day.tsx
@@ -1,8 +1,6 @@
 import { format, addDays } from "date-fns";
 import {DateArray, TimeArray, currentDate, dateFormat, dayFormat} from "./enum";
 
-const data1;
-
 const dateArray = (index=0): { array: DateArray; month: string } => {
 	const days: DateArray = [{ date: "", day: "Time", current: false, fullFormat: new Date() }];
 	for (let i = 0; i < 7; i++) {
