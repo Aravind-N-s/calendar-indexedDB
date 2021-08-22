6d9a6faa404250a3a70947358a16ab29eb3b8a7b
Aravind <aravind1808@gmail.com>
2021-08-22 19:21:14 +0530

    [Calendar][UI] Added App bar and button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
 import "./App.css";
+import {AppBar} from "@material-ui/core";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
 
-function App() {
+const App = (): JSX.Element => {
+
+	const [isWeek, setWeek] = useState<number>(0);
+
+	const onClickHandler = (value: number): void =>	setWeek(isWeek + value);
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography align="right" variant="h6">August</Typography>
+				</Toolbar>
+			</AppBar>
+			<div style={{textAlign: "right",margin: "20px 50px 0px 0px"}}>
+				<ButtonGroup  variant="text" color="primary" aria-label="text primary button group">
+					<Button onClick={() => onClickHandler(-1)}>Previous Week</Button>
+					<Button onClick={() => onClickHandler(1)}>Next Week</Button>
+				</ButtonGroup>
+			</div>
+			<h2>Test</h2>
 		</div>
 	);
-}
+};
 
 export default App;
